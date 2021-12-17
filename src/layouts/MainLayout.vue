393f88f7d6257bc859afbbdfce3a5bf923cf3b26
marllonfernandes <marllon.mfb@gmail.com>
2021-12-17 20:45:33 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,310 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout view="hHh Lpr lff">
+      <q-header elevated class="bg-cyan">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu"/>
+          <q-toolbar-title>TI TOTVS</q-toolbar-title>
+          <div>Quasar v{{ $q.version }}</div>
+          <q-btn flat round dense icon="whatshot"/>
+        </q-toolbar>
+      </q-header>
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :mini="miniState"
+        @mouseover="miniState = false"
+        @mouseout="miniState = true"
+        mini-to-overlay
+        :width="200"
+        :breakpoint="500"
+        bordered
+        class="bg-grey-3"
+      >
+        <q-scroll-area class="fit">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox"/>
+              </q-item-section>
+              <q-item-section>Inbox</q-item-section>
+            </q-item>
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star"/>
+              </q-item-section>
+              <q-item-section>Star</q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="send"/>
+              </q-item-section>
+              <q-item-section>Send</q-item-section>
+            </q-item>
+            <q-separator/>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts"/>
+              </q-item-section>
+              <q-item-section>Drafts</q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+      <q-page-container>
+        <q-page padding>
+          <div class="row">
+            <div class="col-12">
+              <q-table
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+    >
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="name" :props="props">
+            {{ props.row.name }}
+          </q-td>
+          <q-td key="calories" :props="props">
+            <q-badge color="green">
+              {{ props.row.calories }}
+            </q-badge>
+          </q-td>
+          <q-td key="fat" :props="props">
+            <q-badge color="purple">
+              {{ props.row.fat }}
+            </q-badge>
+          </q-td>
+          <q-td key="carbs" :props="props">
+            <q-badge color="orange">
+              {{ props.row.carbs }}
+            </q-badge>
+          </q-td>
+          <q-td key="protein" :props="props">
+            <q-badge color="primary">
+              {{ props.row.protein }}
+            </q-badge>
+          </q-td>
+          <q-td key="sodium" :props="props">
+            <q-badge color="teal">
+              {{ props.row.sodium }}
+            </q-badge>
+          </q-td>
+          <q-td key="calcium" :props="props">
+            <q-badge color="accent">
+              {{ props.row.calcium }}
+            </q-badge>
+          </q-td>
+          <q-td key="iron" :props="props">
+            <q-badge color="amber">
+              {{ props.row.iron }}
+            </q-badge>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+            </div>
+          </div>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+// import EssentialLink from 'components/EssentialLink.vue'
+
+const columns = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'protein', label: 'Protein (g)', field: 'protein' },
+  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+  { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+const rows = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: '14%',
+    iron: '1%'
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: '8%',
+    iron: '1%'
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: '6%',
+    iron: '7%'
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    sodium: 413,
+    calcium: '3%',
+    iron: '8%'
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356,
+    fat: 16.0,
+    carbs: 49,
+    protein: 3.9,
+    sodium: 327,
+    calcium: '7%',
+    iron: '16%'
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375,
+    fat: 0.0,
+    carbs: 94,
+    protein: 0.0,
+    sodium: 50,
+    calcium: '0%',
+    iron: '0%'
+  },
+  {
+    name: 'Lollipop',
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    sodium: 38,
+    calcium: '0%',
+    iron: '2%'
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    sodium: 562,
+    calcium: '0%',
+    iron: '45%'
+  },
+  {
+    name: 'Donut',
+    calories: 452,
+    fat: 25.0,
+    carbs: 51,
+    protein: 4.9,
+    sodium: 326,
+    calcium: '2%',
+    iron: '22%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  }
+]
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    // EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+      drawer: ref(false),
+      miniState: ref(true),
+      columns,
+      rows
+    }
+  }
+})
+</script>
